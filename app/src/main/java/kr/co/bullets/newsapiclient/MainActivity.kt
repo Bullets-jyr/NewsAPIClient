bacd5fa60ec6472bd893bb29a4e593d2193b144b
Bullets-jyr <jyr@bullets.co.kr>
2023-05-28 21:26:40 +0900

    168. Fragments & Navigation Components

--- app/src/main/java/kr/co/bullets/newsapiclient/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/newsapiclient/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.co.bullets.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import kr.co.bullets.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
